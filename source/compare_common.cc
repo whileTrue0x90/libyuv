32053c30f53414291fe4d3724946daca55363e5e
Frank Barchard <fbarchard@google.com>
2017-05-08 15:44:28 -0700

    Optimize Hamming Distance C code to do 64 bits at a time.
    BUG=libyuv:701
    TEST=LibYUVBaseTest.BenchmarkHammingDistance_C
    
    Change-Id: I243003b098bea8ef3809298bbec349ed52a43d8c

--- source/compare_common.cc
+++ source/compare_common.cc
@@ -24,14 +24,22 @@ uint32 HammingDistance_C(const uint8* src_a, const uint8* src_b, int count) {
   int i;
   for (i = 0; i < count; ++i) {
     int x = src_a[i] ^ src_b[i];
-    if (x & 1) ++diff;
-    if (x & 2) ++diff;
-    if (x & 4) ++diff;
-    if (x & 8) ++diff;
-    if (x & 16) ++diff;
-    if (x & 32) ++diff;
-    if (x & 64) ++diff;
-    if (x & 128) ++diff;
+    if (x & 1)
+      ++diff;
+    if (x & 2)
+      ++diff;
+    if (x & 4)
+      ++diff;
+    if (x & 8)
+      ++diff;
+    if (x & 16)
+      ++diff;
+    if (x & 32)
+      ++diff;
+    if (x & 64)
+      ++diff;
+    if (x & 128)
+      ++diff;
   }
   return diff;
 }
@@ -40,14 +48,15 @@ uint32 HammingDistance_C(const uint8* src_a, const uint8* src_b, int count) {
 // Hakmem method for hamming distance.
 uint32 HammingDistance_C(const uint8* src_a, const uint8* src_b, int count) {
   volatile uint32 diff = 0u;
-
   int i;
-  for (i = 0; i < count - 3; i += 4) {
-    uint32 x = *((uint32*)src_a) ^ *((uint32*)src_b);
-    src_a += 4;
-    src_b += 4;
-    uint32 u = x - ((x >> 1) & 033333333333) - ((x >> 2) & 011111111111);
-    diff += ((u + (u >> 3)) & 030707070707) % 63;
+  for (i = 0; i < count - 7; i += 8) {
+    uint64 x = *((uint64*)src_a) ^ *((uint64*)src_b);
+    uint64 u = x - ((x >> 1) & 0x7777777777777777ULL) -
+               ((x >> 2) & 0x3333333333333333ULL) -
+               ((x >> 3) & 0x1111111111111111ULL);
+    diff += ((u + (u >> 4)) & 0x0F0F0F0F0F0F0F0FULL) % 255;
+    src_a += 8;
+    src_b += 8;
   }
   return diff;
 }
