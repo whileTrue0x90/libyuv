94a32666bb5d4072ff09895d2fce91237b1f5032
Frank Barchard <fbarchard@google.com>
2018-01-22 18:12:49 -0800

    basic_types.h - remove unused macros
    Removes macros that were part of standard basic_types
    header but not used by libyuv itself.
    
    TBR=braveyao@chromium.org
    Bug: libyuv:774
    Test: try bots still build
    Change-Id: I8de6fad5a9277df0a50959881392ba212b1b5972

--- source/row_common.cc
+++ source/row_common.cc
@@ -59,7 +59,10 @@ static __inline uint32 Abs(int32 v) {
 }
 #endif  // USE_BRANCHLESS
 
-#ifdef LIBYUV_LITTLE_ENDIAN
+// Little Endian
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || \
+    defined(_M_IX86) || defined(__arm__) || defined(_M_ARM) ||     \
+    (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
 #define WRITEWORD(p, v) *(uint32*)(p) = v
 #else
 static inline void WRITEWORD(uint8* p, uint32 v) {
