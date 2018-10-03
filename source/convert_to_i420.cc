8460f117d0631ca855c0c6d3fcb651ba2f9fc2a3
Moja Hsu <mojahsu@google.com>
2018-10-03 11:37:35 +0800

    Fix ConvertToI420() for odd crop_y
    The original src_u calculation of FOURCC_I420 shifted half width if
    crop_y is odd.
    This CL fixs the problem and also add a test case for it.
    
    Bug: b:115278653
    Test: pass libyuv_unittest
    Change-Id: Ia9732d22e64e13de26df47726ba44ad1c5a06484

--- source/convert_to_i420.cc
+++ source/convert_to_i420.cc
@@ -194,12 +194,12 @@ int ConvertToI420(const uint8_t* sample,
       int halfheight = (abs_src_height + 1) / 2;
       if (format == FOURCC_YV12) {
         src_v = sample + src_width * abs_src_height +
-                (halfwidth * crop_y + crop_x) / 2;
+                halfwidth * (crop_y / 2) + crop_x / 2;
         src_u = sample + src_width * abs_src_height +
                 halfwidth * (halfheight + crop_y / 2) + crop_x / 2;
       } else {
         src_u = sample + src_width * abs_src_height +
-                (halfwidth * crop_y + crop_x) / 2;
+                halfwidth * (crop_y / 2) + crop_x / 2;
         src_v = sample + src_width * abs_src_height +
                 halfwidth * (halfheight + crop_y / 2) + crop_x / 2;
       }
