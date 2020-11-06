2d0d9723c412be7073f22e384236f2d37b86a66b
Hao Chen <chenhao@loongson.cn>
2020-11-06 14:35:20 +0800

    Add supports for loongarch LSX and LASX.
    1. Add supports for LSX and LASX.
    2. Three optimization functions are added in loongarch/row_lasx.cc file:
       I422ToARGBRow_LASX,I422ToRGBARow_LASX,I422AlphaToARGBRow_LASX.
    
    Change-Id: I043c2704f99a5215724b5c0b7f97e6bf5f7a199b

--- unit_test/cpu_test.cc
+++ unit_test/cpu_test.cc
@@ -70,6 +70,15 @@ TEST_F(LibYUVBaseTest, TestCpuHas) {
   int has_mmi = TestCpuFlag(kCpuHasMMI);
   printf("Has MMI %d\n", has_mmi);
 #endif
+
+#if defined(__loongarch__)
+  int has_loongarch = TestCpuFlag(kCpuHasLOONGARCH);
+  printf("Has LOONGARCH %d\n", has_loongarch);
+  int has_lsx = TestCpuFlag(kCpuHasLSX);
+  printf("Has LSX %d\n", has_lsx);
+  int has_lasx = TestCpuFlag(kCpuHasLASX);
+  printf("Has LASX %d\n", has_lasx);
+#endif
 }
 
 TEST_F(LibYUVBaseTest, TestCompilerMacros) {
@@ -148,6 +157,9 @@ TEST_F(LibYUVBaseTest, TestCompilerMacros) {
 #endif
 #ifdef _MIPS_ARCH_LOONGSON3A
   printf("_MIPS_ARCH_LOONGSON3A %d\n", _MIPS_ARCH_LOONGSON3A);
+#endif
+#ifdef __loongarch__
+  printf("__loongarch__ %d\n", __loongarch__);
 #endif
 #ifdef _WIN32
   printf("_WIN32 %d\n", _WIN32);
