c7883704481c472a09f33f60c0d1dbff51882d44
Bruce Lai <bruce.lai@sifive.com>
2023-07-11 19:30:47 -0700

    [RVV] Enable ARGBShuffle_RVV
    1. Add RVV_VRGATHER_MAX_VL check & GetRVVVrgatherMaxVL() in cpu_id
    
       RVV_VRGATHER_MAX_VL indicates how large vl can use when using vrgather.
       GetRVVVrgatherMaxVL is a function to get RVV_VRGATHER_MAX_VL during runtime.
    
    2. Enable ARGBShuffle_RVV
    
       IF VLEN >= 512, using LMUL=1/2 by default. Otherwise, use LMUL=1.
    
    3. [CMake] Support define RVV_VRGATHER_MAX_VL in riscv-clang.cmake
    
    4. [QEMU-script] Support assign VLEN by ENV-var when run riscv_script/run_qemu.sh
    
       Default VLEN is changed from 512 to 128.
    
    * Run on SiFive internal FPGA(VLEN=128):
    
    Test Case	Speedup
    ARGBToRGBA_Opt  4.4
    
    Signed-off-by: Darren Hsieh <darren.hsieh@sifive.com>
    Signed-off-by: Bruce Lai <bruce.lai@sifive.com>

--- riscv_script/run_qemu.sh
+++ riscv_script/run_qemu.sh
@@ -2,12 +2,13 @@
 set -x
 set -e
 
+VLEN="${VLEN:-128}"
 USE_RVV="${USE_RVV:-OFF}"
 TOOLCHAIN_PATH="${TOOLCHAIN_PATH:-../../build-toolchain-qemu/riscv-clang}"
 QEMU_PREFIX_PATH="${QEMU_PREFIX_PATH:-../../build-toolchain-qemu/riscv-qemu/}"
 
 if [ "${USE_RVV}" = "ON" ];then
-  QEMU_OPTION="-cpu rv64,zba=true,zbb=true,zbc=true,zbs=true,v=true,vlen=512,elen=64,vext_spec=v1.0 -L ${TOOLCHAIN_PATH}/sysroot"
+  QEMU_OPTION="-cpu rv64,zba=true,zbb=true,zbc=true,zbs=true,v=true,vlen=${VLEN},elen=64,vext_spec=v1.0 -L ${TOOLCHAIN_PATH}/sysroot"
 else
   QEMU_OPTION="-cpu rv64,zba=true,zbb=true,zbc=true,zbs=true -L ${TOOLCHAIN_PATH}/sysroot"
 fi
